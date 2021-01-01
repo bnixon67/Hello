fcacd65fb22fb61d009bd83cee8ac6f554470990
Bill Nixon <bnixon67@gmail.com>
2021-01-01 16:55:15 -0600

    added printing of command line args

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello Go World!")
+
+	for n := 0; n < len(os.Args); n++ {
+		fmt.Printf("Args[%d] = %s\n", n, os.Args[n])
+	}
 }
