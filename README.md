4736f121326fd603c6dd6c98d9eb8f836017f816
Bill Nixon <bnixon67@gmail.com>
2024-05-14 22:02:09 -0500

    cleanup

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # hello-go
 Hello World program in Go
 
-Prints a simple message, along with the command line arguments
+Prints a simple message, along with the executable path and all of the command
+line arguments.
--- hello.go
+++ hello.go
@@ -1,18 +1,5 @@
-/*
-Copyright 2021 Bill Nixon
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2024 Bill Nixon. All rights reserved.
+// Use of this source code is governed by the license found in the LICENSE file.
 
 package main
 
@@ -22,15 +9,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello Go World!")
+	fmt.Println("hello")
 
 	executable, err := os.Executable()
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("Executable = %s\n", executable)
 
-	for n := 0; n < len(os.Args); n++ {
-		fmt.Printf("Args[%d] = %s\n", n, os.Args[n])
+	fmt.Printf("executable %q\n", executable)
+
+	for n, arg := range os.Args {
+		fmt.Printf("args[%d] %q\n", n, arg)
 	}
 }
